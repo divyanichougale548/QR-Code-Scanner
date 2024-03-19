11bfc23e420b433e20d404686242aec3327c129a
Divyani Sunil Chougale <67824869+divyanichougale548@users.noreply.github.com>
2024-03-19 12:47:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import Scanner from './components/Scanner';
+
+function App() { 
+  return (
+    <div className="App">
+      <h1>QR Code Scanning in React</h1>
+       <Scanner/>
+    </div>
+  );
+}
+
+export default App;
